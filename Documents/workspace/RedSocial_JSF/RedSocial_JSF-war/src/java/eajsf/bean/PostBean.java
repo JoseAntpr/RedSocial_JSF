0eebd0ddea43aba74cc218f38975079982c4f390
Jesus <Jesus@ToshibaL755>
2015-05-25 19:25:44 +0200

    Crear y eliminar post con validador para la imagen <4MB y solo .jpg Falta subir la imagen de post a la BD

--- Documents/workspace/RedSocial_JSF/RedSocial_JSF-war/src/java/eajsf/bean/PostBean.java
+++ Documents/workspace/RedSocial_JSF/RedSocial_JSF-war/src/java/eajsf/bean/PostBean.java
@@ -36,12 +36,17 @@ public class PostBean {
 
     @ManagedProperty(value = "#{loginBean}")
     private LoginBean loginBean;
-
+    
+    @ManagedProperty(value = "#{muroBean}")
+    private MuroBean muroBean;
+    
     @EJB
     private UsuarioFacade usuarioFacade;
 
     @EJB
     private PostFacade postFacade;
+    
+    List<FacesMessage> msgs;
 
     private String text = null;
     private Part img = null;
@@ -100,20 +105,47 @@ public class PostBean {
     public void setImgContent(String imgContent) {
         this.imgContent = imgContent;
     }
-    
+
+    public List<FacesMessage> getMsgs() {
+        return msgs;
+    }
+
+    public void setMsgs(List<FacesMessage> msgs) {
+        this.msgs = msgs;
+    }
+
+    public MuroBean getMuroBean() {
+        return muroBean;
+    }
+
+    public void setMuroBean(MuroBean muroBean) {
+        this.muroBean = muroBean;
+    }
+
+
     
 
     public void validarImagen(FacesContext ctx, UIComponent comp, Object value) {
-        List<FacesMessage> msgs = new ArrayList<FacesMessage>();
-        Part file = (Part) value;
-        if ((file.getSize() / (1024)) > 4096) {
-            msgs.add(new FacesMessage("Sólo se permiten imágenes con tamaño inferior a 4Mb"));
-        }
-        if (!"image/jpeg".equals(file.getContentType())) {
-            msgs.add(new FacesMessage("Solo se admiten imágenes .jpg"));
-        }
-        if (!msgs.isEmpty()) {
-            throw new ValidatorException(msgs);
+        msgs = new ArrayList<FacesMessage>();
+        
+        if(value!=null){
+            Part file = (Part) value;
+
+            for (FacesMessage msg : msgs) {
+                msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+    //            FacesContext.getCurrentInstance().addMessage("id1", errorMessage);
+            }
+
+            if ((file.getSize() / (1024)) > 4096) {
+                msgs.add(new FacesMessage("Sólo se permiten imágenes con tamaño inferior a 4Mb"));
+            }
+            if (!"image/jpeg".equals(file.getContentType())) {
+                msgs.add(new FacesMessage("Solo se admiten imágenes .jpg"));
+            }
+            if (!msgs.isEmpty()) {
+                throw new ValidatorException(msgs);
+            }
+        
         }
     }
 
@@ -149,6 +181,10 @@ public class PostBean {
         // Actualizamos la relacion USUARIO-POST (MURO)
         usuarioFacade.edit(usuario);
 
+
+//        muroBean.setListaPost(postFacade.findPostIdUsuarioOrder(loginBean.getUsuario().getIdUsuario()));
+        
+        
         return "muro";
     }
     
@@ -163,14 +199,15 @@ public class PostBean {
         return null;
     }
 
-    public String borrarPost(BigDecimal idPost) {
+    public String borrarPost(Post p) {
 
-        postFacade.deletePost(idPost);
-
+        postFacade.deletePost(p.getIdPost());
+        loginBean.getUsuario().getPostCollection().remove(p);
+        usuarioFacade.edit(loginBean.getUsuario());
+        
+        //        muroBean.setListaPost(postFacade.findPostIdUsuarioOrder(loginBean.getUsuario().getIdUsuario()));
+        
         return "muro";
     }
-
-    public String redirectAddPost() {
-        return "postAdd";
-    }
+    
 }
