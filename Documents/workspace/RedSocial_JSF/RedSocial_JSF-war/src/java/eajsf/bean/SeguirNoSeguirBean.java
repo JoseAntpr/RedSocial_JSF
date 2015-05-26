3261c29a11f1b3dd03c5b813c16b820a3bec8b85
Jesus <Jesus@ToshibaL755>
2015-05-26 17:46:04 +0200

    Botones seguir siguiendo

--- Documents/workspace/RedSocial_JSF/RedSocial_JSF-war/src/java/eajsf/bean/SeguirNoSeguirBean.java
+++ Documents/workspace/RedSocial_JSF/RedSocial_JSF-war/src/java/eajsf/bean/SeguirNoSeguirBean.java
@@ -22,13 +22,70 @@ public class SeguirNoSeguirBean {
     
     @ManagedProperty(value = "#{loginBean}")
     private LoginBean loginBean;
+    private String titulo;
+    private String classBoton;
     
-    Usuario usuario;
+    private Usuario usuario;
 
     /**
      * Creates a new instance of SeguirNoSeguirBean
      */
     public SeguirNoSeguirBean() {
     }
+
+    public LoginBean getLoginBean() {
+        return loginBean;
+    }
+
+    public void setLoginBean(LoginBean loginBean) {
+        this.loginBean = loginBean;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClassBoton() {
+        return classBoton;
+    }
+
+    public void setClassBoton(String classBoton) {
+        this.classBoton = classBoton;
+    }
+    
+    
+
+    public String tituloBoton(Usuario u){
+        this.usuario=u;
+        if(loginBean.getUsuario().siguesUsuario(u)){
+            this.titulo="Siguiendo";
+        }else{
+            this.titulo="Seguir";
+        }
+        return titulo;
+    }
+    
+    public String classBoton(){
+        if(loginBean.getUsuario().siguesUsuario(this.usuario)){
+            this.classBoton="btn btn-success";
+        }else{
+            this.classBoton="btn btn-primary";
+        }
+        return classBoton;
+    }
+    
+    
    
 }
