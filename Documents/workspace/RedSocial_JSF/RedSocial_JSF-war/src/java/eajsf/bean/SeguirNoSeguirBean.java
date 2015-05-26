0bbdd49a677640dc4bb2f16a5dbf095ad6555cf2
Jose Antonio Palacios Ramírez <josepio11.uma@gmail.com>
2015-05-26 09:46:42 +0200

    Listar usuarios y mostrarlos Boton Seguir no seguir casi implentado

--- Documents/workspace/RedSocial_JSF/RedSocial_JSF-war/src/java/eajsf/bean/SeguirNoSeguirBean.java
+++ Documents/workspace/RedSocial_JSF/RedSocial_JSF-war/src/java/eajsf/bean/SeguirNoSeguirBean.java
@@ -1,20 +1,28 @@
+package eajsf.bean;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package eajsf.bean;
 
+import eajsf.entity.Usuario;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
 /**
  *
- * @author fran
+ * @author Joseantpr
  */
 @ManagedBean
 @RequestScoped
 public class SeguirNoSeguirBean {
+    
+    @ManagedProperty(value = "#{loginBean}")
+    private LoginBean loginBean;
+    
+    Usuario usuario;
 
     /**
      * Creates a new instance of SeguirNoSeguirBean
